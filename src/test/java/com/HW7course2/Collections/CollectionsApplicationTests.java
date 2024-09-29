fc9295593aea8ad17576a6d289aa6d3929457bf1
Neytred <ls.ivanova91@gmail.com>
2024-09-29 20:45:46 +0900

    Initial commit

---
+++ src/test/java/com/HW7course2/Collections/CollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HW7course2.Collections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
